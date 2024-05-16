afdfb0c02759b8d6ba21f7794f2951f2e7c45351
Rafael Timoteo <rafaeltimoteolima1202@gmail.com>
2024-05-16 16:16:57 -0300

    Primeiro commit

---
+++ src/main/java/br/com/storemanager/storemanagerapi/StoremanagerApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.storemanager.storemanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoremanagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoremanagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/storemanager/storemanagerapi/StoremanagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.storemanager.storemanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoremanagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
