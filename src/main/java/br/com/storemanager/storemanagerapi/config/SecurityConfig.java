22203f8bd49b8bbad87e9f3ed4458c7ebc23e43e
Rafael Timoteo <rafaeltimoteolima1202@gmail.com>
2024-06-11 12:16:25 -0300

    configuração da classe SecurityConfig

--- src/main/java/br/com/storemanager/storemanagerapi/config/SecurityConfig.java
+++ src/main/java/br/com/storemanager/storemanagerapi/config/SecurityConfig.java
@@ -1,12 +1,64 @@
 package br.com.storemanager.storemanagerapi.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.http.HttpMethod;
+
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+    
+    private static final String[] PUBLIC_MATCHERS = {
+        "/"
+    };
+
+    private static final String[] PUBLIC_MATCHERS_POST = {
+        "/user",
+        "/login"
+    };
+
+    @Bean
+    public SecurityFilterChain  filterChain(HttpSecurity http) throws Exception {
+
+        http
+        .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
+            .requestMatchers(PUBLIC_MATCHERS).permitAll()
+            .requestMatchers("/error").permitAll() 
+            .anyRequest().authenticated()   
+        )
+        .csrf(csrf -> csrf
+            .disable()
+        )
+        .sessionManagement(managment -> managment
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+        
+        return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+        configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
