0d0e62f63f8c1f540a9b55bbff1f11a925298239
Rafael Timoteo <rafaeltimoteolima1202@gmail.com>
2024-06-27 16:01:02 -0300

    criado as configurações para autenticação

--- src/main/java/br/com/storemanager/storemanagerapi/config/SecurityConfig.java
+++ src/main/java/br/com/storemanager/storemanagerapi/config/SecurityConfig.java
@@ -2,26 +2,40 @@ package br.com.storemanager.storemanagerapi.config;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.http.HttpMethod;
 
+import br.com.storemanager.storemanagerapi.security.JWTAuthenticationFilter;
+import br.com.storemanager.storemanagerapi.security.JWTUtil;
+
+import org.springframework.http.HttpMethod;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
-    
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
     private static final String[] PUBLIC_MATCHERS = {
         "/"
     };
@@ -32,26 +46,36 @@ public class SecurityConfig {
     };
 
     @Bean
-    public SecurityFilterChain  filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
 
         http
         .authorizeHttpRequests(authorize -> authorize
             .requestMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
             .requestMatchers(PUBLIC_MATCHERS).permitAll()
-            .requestMatchers("/error").permitAll() 
-            .anyRequest().authenticated()   
+            .requestMatchers("/error").permitAll()
+            .anyRequest().authenticated()
         )
         .csrf(csrf -> csrf
             .disable()
         )
-        .sessionManagement(managment -> managment
+        .addFilterBefore(new JWTAuthenticationFilter(authenticationManager, jwtUtil), UsernamePasswordAuthenticationFilter.class)
+        .sessionManagement(management -> management
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         );
-        
+
         return http.build();
     }
 
     @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = 
+            http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService)
+            .passwordEncoder(passwordEncoder());
+        return authenticationManagerBuilder.build();
+    }
+
+    @Bean   
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
         configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE"));
@@ -62,8 +86,6 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(); 
     }
-
 }
-
