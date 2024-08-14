5ac9370a8b9337e71b9ad98aa2ca23d7defb35a7
Rafael Timoteo <rafaeltimoteolima1202@gmail.com>
2024-08-14 13:38:57 -0300

    tirado as importações do cors

--- src/main/java/br/com/storemanager/storemanagerapi/config/SecurityConfig.java
+++ src/main/java/br/com/storemanager/storemanagerapi/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package br.com.storemanager.storemanagerapi.config;
 
-import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -16,9 +15,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import br.com.storemanager.storemanagerapi.security.JWTAuthenticationFilter;
 import br.com.storemanager.storemanagerapi.security.JWTAuthorizationFilter;
