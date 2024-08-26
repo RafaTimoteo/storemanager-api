9f28cb7f26411baedb6d127c870134ec8d0e22b2
Rafael Timoteo <rafaeltimoteolima1202@gmail.com>
2024-08-26 00:13:00 -0300

    criado metodo que busca todos os fornecedores da tabela

--- src/main/java/br/com/storemanager/storemanagerapi/repositories/FornecedorRepository.java
+++ src/main/java/br/com/storemanager/storemanagerapi/repositories/FornecedorRepository.java
@@ -1,6 +1,9 @@
 package br.com.storemanager.storemanagerapi.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.storemanager.storemanagerapi.models.Fornecedor;
@@ -8,4 +11,6 @@ import br.com.storemanager.storemanagerapi.models.Fornecedor;
 @Repository
 public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
 
+    @Query("select t from Fornecedor t")
+    public List<Fornecedor> getAllFornecedores();
 }
