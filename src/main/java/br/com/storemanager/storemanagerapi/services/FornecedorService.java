c0a2fc708174a7fb373cad83dc468c9d2ac056c4
Rafael Timoteo <rafaeltimoteolima1202@gmail.com>
2024-08-26 00:14:10 -0300

    criado metodo que lida com o retorno de todos os fornecedores da lista

--- src/main/java/br/com/storemanager/storemanagerapi/services/FornecedorService.java
+++ src/main/java/br/com/storemanager/storemanagerapi/services/FornecedorService.java
@@ -1,5 +1,6 @@
 package br.com.storemanager.storemanagerapi.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,12 @@ public class FornecedorService {
     
     @Autowired
     private FornecedorRepository fornecedorRepository;
+
+    public List<Fornecedor> getAllFornecedores() {
+        List<Fornecedor> fornecedores = fornecedorRepository.getAllFornecedores();
+
+        return fornecedores;
+    }
 
     // Retorna o um fornecedor pelo ID
     public Fornecedor findFornecedorById(Long id) {
